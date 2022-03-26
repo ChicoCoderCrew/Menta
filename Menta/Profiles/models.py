8eb1c7e5677c49b7538e52a5b01a65c4a2eaf801
Reema <reema.a.shadid@gmail.com>
2022-03-26 00:01:09 -0700

    edited Profile model for first and last name entry, and URL entry

--- Menta/Profiles/models.py
+++ Menta/Profiles/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Profile(models.Model):
 
-    name= models.CharField(verbose_name= 'First and Last name', max_length=100)
-
+    firstName= models.CharField(verbose_name= 'First Name', max_length=100)
+    lastName= models.CharField(verbose_name= 'Last Name', max_length=100)
 
     TYPE_CHOICES = [
         ('MO', 'Mentor'),
@@ -14,6 +14,8 @@ class Profile(models.Model):
     userType= models.CharField(verbose_name='Mentor/Mentee', choices= TYPE_CHOICES, max_length=100)
 
     occupation= models.CharField(verbose_name= 'Occupation', max_length=100)
+
+    website= models.URLField(verbose_name='Professional Portfolio URL (GitHub, LinkedIn, etc.)')
     
     age= models.PositiveSmallIntegerField(verbose_name= 'Age')
 
@@ -27,3 +29,15 @@ class Profile(models.Model):
     biography= models.TextField(verbose_name='Personal Bio', max_length=2000)
 
     photos= models.ImageField(verbose_name='Headshot')
+
+# #save a profile
+# newProfile = Profile(firstName='n00b', userType='Mentee', age= 0, gender= 'Other', biography= 'i like to hack, and sack.')
+# newProfile.save()
+
+# #retrieve an object
+# Profile.objects
+# pulledProfile = Profile(firstName= 'n00b')
+# pulledProfile.objects
+
+# #retrieve all objects
+# allProfiles = Profile.objects.all()
