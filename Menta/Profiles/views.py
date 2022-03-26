1ef541eca4e2cc645168d5ed276feeb8583c5272
Parth Pandey <parthpandey1808@gmail.com>
2022-03-26 03:30:11 -0700

    Views and serializer

--- Menta/Profiles/views.py
+++ Menta/Profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+from rest_framework import viewsets
+
+from .serializers import ProfileSerializer
+from .models import Profile
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all().order_by('name')
+    serializer_class = ProfileSerializer
