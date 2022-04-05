50729153a6f504add822786d5284dd350a546468
Jooms <jamesKrepelka@gmail.com>
2022-04-05 14:00:48 -0700

    Merge new frontend code (#20)
    * api integration for profile
    
    * api integration for profile 2
    
    * profiule got working
    
    * bla
    
    Co-authored-by: AbhinavReddy-Dev <abhinavreddy9681@gmail.com>

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { colorScheme } from "../theme";
 
 const Home = () => {
-  return <h1 style={{ margin: 0 }}>Welcome to Menta!</h1>;
+  const [ID, setID] = useState(null);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    console.log(ID);
+    navigate("./profile", { state: { id: ID } });
+  };
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <h1 style={{ margin: 0 }}>Welcome to Menta!</h1>
+
+      <div
+        style={{
+          margin: 0,
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "3rem",
+        }}>
+        <label
+          style={{ color: "gray", fontSize: "24px", marginBottom: "1rem" }}>
+          Enter your ID
+        </label>
+        <div
+          style={{
+            position: "relative",
+            // border: "solid 2px gray",
+            width: "fit-content",
+            margin: "1rem auto",
+          }}>
+          <input
+            type="number"
+            style={{
+              alignSelf: "center",
+              maxWidth: "220px",
+              border: "none",
+              backgroundColor: colorScheme.lightDarkOrange,
+              borderRadius: "5px",
+              fontSize: "20px",
+              color: colorScheme.white,
+              padding: "15px 10px",
+            }}
+            onChange={(e) => setID(e.target.value)}
+          />
+
+          <button
+            style={{
+              height: "42px",
+              width: "45px",
+              position: "absolute",
+              color: "white",
+              fontSize: "20px",
+              borderRadius: "50%",
+              backgroundColor: "rgb(4, 170, 109)",
+              right: 5,
+              top: 5,
+              border: "none",
+              cursor: "pointer",
+            }}
+            type="submit"
+            onClick={handleSubmit}>
+            Go!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { colorScheme } from "../theme";
-
+import axios from "axios";
 // https://picsum.photos/seed/{user.id}/200/300
 
 const Profile = () => {
+  const location = useLocation();
+  const state = location.state;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  // "name": "Some Guy",
+  // "userType": "MO",
+  // "occupation": "Software Development Engineer",
+  // "website": "http://www.google.com",
+  // "age": 45,
+  // "gender": "M",
+  // "bio": "I don't really have much to say.... I'm just Some Guy.",
+  // "skills": [
+  // "Javascript",
+  // "C++"
+  // ]
+
+  let headers = new Headers();
+
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+  // headers.append("Access-Control-Allow-Origin", "*");
+  // headers.append("Origin", "http://localhost:3000");
+
+  useEffect(() => {
+    if (state) {
+      try {
+        axios
+          .get(`http://34.132.172.130/user/view/${state.id}`)
+          .then(function (response) {
+            console.log(response.data);
+            setUser(response.data);
+          })
+          .catch((e) => {
+            console.log(e);
+            navigate("/");
+          });
+      } catch (e) {
+        navigate("/");
+      }
+    } else {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div
       style={{
@@ -12,40 +57,64 @@ const Profile = () => {
         color: colorScheme.white,
       }}>
       <h2>User Profile</h2>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          // alignItems: "flex-start",
-        }}>
-        <img
-          src="https://picsum.photos/seed/random/200/200"
-          alt="Girl in a jacket"
-          width="100"
-          height="100"
+      {user && (
+        <div
           style={{
-            borderRadius: "50%",
-            alignSelf: "center",
-            marginBottom: "24px",
-          }}
-        />
-        <label style={{ color: "gray", fontSize: "14px" }}>Name</label>
-        {1 ? <p>{"Bla Bla"}</p> : <input />}
-        <label style={{ color: "gray", fontSize: "14px" }}>Email</label>
-        {1 ? <p>{"blabla@blamail.bla"}</p> : <input />}
-        <label style={{ color: "gray", fontSize: "14px" }}>Occupation</label>
-        {1 ? <p>{"Bla Maker"}</p> : <input />}
-        <label style={{ color: "gray", fontSize: "14px" }}>Bio</label>
-        {1 ? (
-          <p>
-            {
-              "BlaBla Bla bla blablabla...Bla bla blablabla...Bla bla blablabla...Bla bla blablabla...Bla bla blablabla..."
-            }
-          </p>
-        ) : (
-          <input />
-        )}
-      </div>
+            display: "flex",
+            flexDirection: "column",
+            // alignItems: "flex-start",
+          }}>
+          <img
+            src={`https://picsum.photos/seed/${user.id}/200/200`}
+            alt="Girl in a jacket"
+            width="100"
+            height="100"
+            style={{
+              borderRadius: "50%",
+              alignSelf: "center",
+              marginBottom: "24px",
+            }}
+          />
+          <label style={{ color: "gray", fontSize: "14px" }}>Name</label>
+          {1 ? <p>{user.name}</p> : <input />}
+          {/* <label style={{ color: "gray", fontSize: "14px" }}>Email</label>
+        {1 ? <p>{"blabla@blamail.bla"}</p> : <input />} */}
+          <label style={{ color: "gray", fontSize: "14px" }}>Occupation</label>
+          {1 ? <p>{user.occupation}</p> : <input />}
+          <label style={{ color: "gray", fontSize: "14px" }}>Bio</label>
+          {1 ? <p>{user.bio}</p> : <input />}
+
+          <label style={{ color: "gray", fontSize: "14px" }}>Skills</label>
+          {1 ? (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}>
+              {user.skills.map((skill, i) => (
+                <p
+                  key={i}
+                  style={{
+                    color: "#fff",
+                    fontSize: "12px",
+                    padding: "2px 4px",
+                    marginRight: "5px",
+                    backgroundColor: "#916BBF",
+                    height: "fit-content",
+                    width: "fit-content",
+
+                    borderRadius: "5px",
+                  }}>
+                  {skill}
+                </p>
+              ))}
+            </div>
+          ) : (
+            <input />
+          )}
+        </div>
+      )}
     </div>
   );
 };
